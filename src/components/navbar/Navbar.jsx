1f64a9086a3ff447b9a94a42c8981c5df2615293
Milhan Osman <milhanomarosman@gmail.com>
2023-07-03 16:53:13 +0300

    navbar skeleton structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,36 @@
-import "./navbar.css";
+
+// import React from "react";
+import "./navbar.css"
+import ImgPlaceHolder from "../../assets/img-placeholder.png"
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+
+    <div className="nav-wrapper">
+      <h1 className="header">Readers Haven</h1>
+      <ul id="nav">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Search</a>
+        </li>
+        <li>
+          <a href="#">Genres</a>
+        </li>
+        <li>
+          <a href="#">Authors</a>
+        </li>
+         <li>
+          <a href="#">Best Selling</a>
+        </li>
+         <li>
+          {/* <a href="#">Best Selling</a> */}
+          <img src={ImgPlaceHolder} className="img-placeholder" />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
