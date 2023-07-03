99935f55d03501165e288721fa6181935407e429
felixNyalenda <felixasejunior@gmail.com>
2023-07-03 23:31:27 +0300

    footer refactor

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
 import "./footer.css";
 
 const Footer = () => {
   return (
-      <div className="main-footer">
-        <div className="container">
-          <div className="row">
-            {/* column 1 */}
-            <div className="col">
-              <h4>unwritten yet</h4>
-              <ul className="list-unstyled">
-                <li>456465w5745</li>
-                <li>dsdfbzgfbf</li>
-                <li>dgsdbxfgfb</li>
-              </ul>
-            </div>
-            {/* column 2 */}
-            <div className="col">
-              <h4>welcome</h4>
-              <ul className="list-unstyled">
-                <li>dfrgergb</li>
-                <li>dfgzdfgzdfgz</li>
-                <li>zdfgzdfnzdfh</li>
-              </ul>
-            </div>
-            {/* column 3 */}
-            <div className="col">
-              <h4>full enough</h4>
-              <ul className="list-unstyled">
-                <li>zdfgbzdfbz</li>
-                <li>dfgzdfgdgadf</li>
-                <li>zfvardgdggd</li>
-              </ul>
-            </div>
-          </div>
-          <hr />
-          <div className="row">
-            <p className="col-sm">
-              &copy;{new Date().getFullYear} All right reserved| Terms of service | Privacy
-            </p>
-          </div>
+    <footer className="footer">
+      <div className="footer-links-container">
+        {/* column 1 */}
+        <div className="footer-links">
+          <h4>Company</h4>
+          <ul className="links">
+            <li>About</li>
+            <li>Careers</li>
+            <li>Brand Center</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+
+        {/* column 2 */}
+        <div className="footer-links">
+          <h4>Help & Support</h4>
+          <ul className="links">
+            <li>Contact Us</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+
+        {/* column 3 */}
+        <div className="footer-links">
+          <h4>Download</h4>
+          <ul className="links">
+            <li>iOS</li>
+            <li>Android</li>
+            <li>Windows</li>
+            <li>MacOS</li>
+          </ul>
         </div>
       </div>
+
+      <p className="col-sm">
+        &copy; {new Date().getFullYear} All right reserved | Terms of service |
+        Privacy
+      </p>
+    </footer>
   );
 };
 
