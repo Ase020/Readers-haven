2d23f12f3d339b95ad8ddea7f207e2d17ad8e8c8
felixNyalenda <felixasejunior@gmail.com>
2023-07-03 17:43:10 +0300

    password-reset page build

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import cover1 from "./cover1.jpeg";
 import cover2 from "./cover2.jpeg";
+import cover3 from "./cover3.jpeg";
 
-export { cover1, cover2 };
+export { cover1, cover2, cover3 };
--- src/pages/password-reset/PasswordReset.jsx
+++ src/pages/password-reset/PasswordReset.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
 import "./password-reset.css";
+import { cover3 } from "../../assets";
 
 const PasswordReset = () => {
-  return <div>PasswordReset</div>;
+  return (
+    <div className="reset">
+      <div className="reset-container">
+        <div className="reset-welcome-page">
+          <img src={cover3} alt="cover" className="reset-img" />
+
+          <div className="welcome-text">
+            <h1 className="welcome-text-title">
+              Readers <br />
+              Haven
+            </h1>
+
+            <p className="welcome-text-desc">
+              Welcome to the best platform on the web for your favorite book
+              reviews trusted and approved by thousands of reknowned authors and
+              publishers.
+            </p>
+
+            <div className="reset-wrapper">
+              <p className="reset-text">Back to login</p>
+              <button>
+                <Link to="/login">Log in</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="reset-form-wrapper">
+          <h1 className="title">Rest password</h1>
+          <form className="reset-form">
+            <input type="text" placeholder="Username" className="reset-input" />
+
+            <input
+              type="password"
+              placeholder="New password"
+              className="reset-input"
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className="reset-input"
+            />
+            <button type="submit" className="reset-btn">
+              Reset
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PasswordReset;
