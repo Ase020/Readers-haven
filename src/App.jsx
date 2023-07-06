cc4c7155c52f40b7a4165547d462bba7342b8717
Milhan Osman <milhanomarosman@gmail.com>
2023-07-06 10:20:53 +0300

    revamping

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import "./App.css";
-import { Home, Layout, Login, PasswordReset, Signup, Search } from "./pages";
+import './App.css';
+import {
+  Home,
+  Layout,
+  Login,
+  PasswordReset,
+  Signup,
+  Search,
+  Authors,
+} from './pages';
+// import Authors from "./pages/author/Authors";
+// import Author from "./pages/author/Authors";
 
 const App = () => {
+  const [allBook, setAllBook] = useState([]);
+  const getAllbook = async () => {
+    const res = await fetch(
+      'https://peaceful-oasis-68149-c720121aea60.herokuapp.com/books'
+    );
+    const data = await res.json();
+    setAllBook(data);
+    console.log(data);
+  };
+  useEffect(() => {
+    getAllbook();
+  }, []);
+
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Layout />,
       children: [
         {
-          path: "/",
-          element: <Home />,
+          path: '/',
+          element: <Home allBook={allBook} />,
         },
         {
-          path: "/login",
+          path: '/login',
           element: <Login />,
         },
         {
-          path: "/signup",
+          path: '/signup',
           element: <Signup />,
         },
         {
-          path: "/login/password_reset",
+          path: '/login/password_reset',
           element: <PasswordReset />,
         },
         {
-          path: "/search",
-          element: <Search />,
+          path: '/search',
+          element: <Search allBook={allBook} />,
+        },
+        {
+          path: '/authors',
+          element: <Authors />,
         },
         //  {
         //   path: "/genres",
