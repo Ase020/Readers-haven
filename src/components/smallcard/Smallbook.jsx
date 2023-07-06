06aa822d6debc8a8f8671026ef04f78b2a7fb20d
Milhan Osman <milhanomarosman@gmail.com>
2023-07-06 10:20:20 +0300

    created smallbooks component

---
+++ src/components/smallcard/Smallbook.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import './book-card.css';
+
+// eslint-disable-next-line react/prop-types
+function SmallBook({ title, publish_date, price, image }) {
+  return (
+    <div className="small-book-card">
+      <img className="small-book-img" src={image} alt={title} />
+      <div className="card-info">
+        <h2 className="title">{title}</h2>
+        {/* <p>Author: {author}</p> */}
+        <p>Published in: {publish_date}</p>
+        <p>Price: ${price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SmallBook;
