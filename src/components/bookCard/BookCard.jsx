1e5700ca03c33eb55f0c67d220cb61a68fb93e13
Milhan Osman <milhanomarosman@gmail.com>
2023-07-06 10:18:58 +0300

    refactaring books

--- src/components/bookCard/BookCard.jsx
+++ src/components/bookCard/BookCard.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import "./book-card.css";
+// import { Link } from 'react-router-dom';
+import './book-card.css';
 
-const BookCard = ({ author, title, rating, price, image }) => (
+const BookCard = ({ title, publish_date, price, image }) => (
   <div className="book-card">
     <img src={image} alt={title} />
     <h2 className="title">{title}</h2>
-    <p><Link to="/author">Author: {author}</Link></p>
-    <p>Rating: {rating}</p>
+    {/* <p>
+      <Link to="/author">Author: {author}</Link>
+    </p> */}
+    <p>publish_date: {publish_date}</p>
     <p>Price: ${price}</p>
     <button>More Information</button>
-  </div> 
+  </div>
 );
 
 export default BookCard;
