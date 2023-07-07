c6cfc1b39d90fc56986a118ea9282515494c95e7
felixNyalenda <felixasejunior@gmail.com>
2023-07-07 14:01:12 +0300

    add form

--- src/pages/add-book/AddBook.jsx
+++ src/pages/add-book/AddBook.jsx
@@ -1,7 +1,54 @@
 import "./add-book.css";
 
 const AddBook = () => {
-  return <div>AddBook</div>;
+  return (
+    <div className="add_book-container">
+      <div className="add_book-wrapper">
+        <h1 className="add_book-header">Add a Book for a review</h1>
+        <form className="add_book-form">
+          <input
+            type="text"
+            placeholder="Title"
+            className="add_book-form-input"
+          />
+          <input
+            type="text"
+            placeholder="Genre"
+            className="add_book-form-input"
+          />
+          <input
+            type="number"
+            placeholder="Price"
+            className="add_book-form-input"
+          />
+          <input
+            type="text"
+            placeholder="Poster url"
+            className="add_book-form-input"
+          />
+          <input
+            type="number"
+            placeholder="Author_id"
+            className="add_book-form-input"
+          />
+          <input
+            type="number"
+            placeholder="Publisher_id"
+            className="add_book-form-input"
+          />
+          <input
+            type="number"
+            placeholder="User_id"
+            className="add_book-form-input"
+          />
+
+          <button type="submit" className="add_book-form-btn">
+            Add book
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddBook;
