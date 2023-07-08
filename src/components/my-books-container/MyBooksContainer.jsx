e0e8a9a4e8f685d965e8e23b6352aff5a3842132
felixNyalenda <felixasejunior@gmail.com>
2023-07-09 01:33:17 +0300

    map books to profile page

--- src/components/my-books-container/MyBooksContainer.jsx
+++ src/components/my-books-container/MyBooksContainer.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { EditOutlined, DeleteOutlined, AddOutlined } from "@mui/icons-material";
 
 import "./my-books-container.css";
 
-const MyBooksContainer = () => {
+const NoBook = () => (
+  <h1 className="add-book-to-shelf">Add Books for reviews</h1>
+);
+
+const MyBooksContainer = ({ user }) => {
   return (
     <div className="my-books-container">
       <div className="my-books-header">
@@ -18,96 +23,20 @@ const MyBooksContainer = () => {
       </div>
 
       <div className="my-books-wrapper">
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
+        {user.books.length > 0 ? (
+          user.books.map((book) => (
+            <div className="my-book-wrapper" key={book?.id}>
+              <Link to={`/books/${book.id}`}>{book?.title}</Link>
 
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
-        <div className="my-book-wrapper">
-          <Link to={"/"}>Moringa Syllabus</Link>
-
-          <div className="my-book-btn-wrapper">
-            <EditOutlined className="my-book-btn" />
-            <DeleteOutlined className="my-book-btn" />
-          </div>
-        </div>
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+          ))
+        ) : (
+          <NoBook />
+        )}
       </div>
     </div>
   );
