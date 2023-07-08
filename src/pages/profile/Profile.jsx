4f0d5f244d6c9b14de25f6a11c09cdc2d4b7f61c
felixNyalenda <felixasejunior@gmail.com>
2023-07-09 00:24:34 +0300

    redirect to login if user logged in

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+
 import { MyBooksContainer, MyProfileContainer } from "../../components";
+import { UserContext } from "../../context/user";
 import "./profile.css";
 
 const Profile = () => {
+  const [user] = useContext(UserContext);
+
+  if (isNaN(user.id)) {
+    // alert("You have to be logged in to view this page!");
+    window.location.href = "/login";
+  }
+
+  console.log("user: ", user);
   return (
     <div className="profile-wrapper">
       <h1 className="profile-welcome">
