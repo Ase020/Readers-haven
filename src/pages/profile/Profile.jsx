74acee7a1dfbffacd9169742fecf01c273d13595
felixNyalenda <felixasejunior@gmail.com>
2023-07-08 23:52:34 +0300

    my books section build

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
-import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { EditOutlined, DeleteOutlined } from "@mui/icons-material";
 
 import "./profile.css";
 
@@ -45,14 +44,95 @@ const Profile = () => {
             <span className="my-books-header-span">My </span>Books
           </h3>
 
-          <div className="my-books-container">
-            <div className="my-book-container">
+          <div className="my-books-wrapper">
+            <div className="my-book-wrapper">
               <Link to={"/"}>Moringa Syllabus</Link>
 
               <div className="my-book-btn-wrapper">
-                <button>
-                  <EditOutlinedIcon />
-                </button>
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
+              </div>
+            </div>
+            <div className="my-book-wrapper">
+              <Link to={"/"}>Moringa Syllabus</Link>
+
+              <div className="my-book-btn-wrapper">
+                <EditOutlined className="my-book-btn" />
+                <DeleteOutlined className="my-book-btn" />
               </div>
             </div>
           </div>
