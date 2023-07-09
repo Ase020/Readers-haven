18ab8e5485321bb567827324b12e250ad5f6d4c0
felixNyalenda <felixasejunior@gmail.com>
2023-07-09 15:09:31 +0300

    fetch and map author books

--- src/components/author-books/AuthorBooks.jsx
+++ src/components/author-books/AuthorBooks.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import "./author-books.css";
-import { AddOutlined, DeleteOutlined, EditOutlined } from "@mui/icons-material";
+import { AddOutlined } from "@mui/icons-material";
 
 const NoBook = () => (
   <h1 className="add-book-to-shelf">Add Books for reviews</h1>
 );
 
-const AuthorBooks = () => {
+const AuthorBooks = ({ books }) => {
   return (
-    <div className="my-books-container">
-      <div className="my-books-header">
-        <h3 className="my-books-header-span">
-          My <span> Books</span>
+    <div className="author-books-container">
+      <div className="author-books-header">
+        <h3 className="author-books-header-span">
+          Authored <span> Books</span>
         </h3>
         <Link to="/books/add">
           <AddOutlined
@@ -20,24 +21,20 @@ const AuthorBooks = () => {
         </Link>
       </div>
 
-      <div className="my-books-wrapper">
-        {/* {user1.books.length > 0 ? (
-          user1.books.map((book) => (
-            <div className="my-book-wrapper" key={book?.id}>
+      <div className="author-books-wrapper">
+        {books?.length > 0 ? (
+          books?.map((book) => (
+            <div className="author-book-wrapper" key={book?.id}>
               <Link to={`/books/${book.id}`}>{book?.title}</Link>
 
-              <div className="my-book-btn-wrapper">
-                <EditOutlined className="my-book-btn" />
-                <DeleteOutlined
-                  className="my-book-btn"
-                  onClick={() => handleDelete(book.id)}
-                />
+              <div className="author-book-btn-wrapper">
+                Genre: &nbsp;<span>{book.genre}</span>
               </div>
             </div>
           ))
         ) : (
           <NoBook />
-        )} */}
+        )}
       </div>
     </div>
   );
